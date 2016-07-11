a931ac1d6f185ecdd33cbe15f598e360c04fe698
Jaxlying <Jaxlying@outlook.com>
2016-07-11 22:31:21 +0800

    修改配置

--- src/main/java/cn/edu/upc/yb/integrate/calendar/controller/CalendarAuthController.java
+++ src/main/java/cn/edu/upc/yb/integrate/calendar/controller/CalendarAuthController.java
@@ -3,6 +3,7 @@ package cn.edu.upc.yb.integrate.calendar.controller;
 import cn.edu.upc.yb.integrate.calendar.config.CalendarConfig;
 import cn.edu.upc.yb.integrate.common.auth.YibanOAuth;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +15,12 @@ public class CalendarAuthController {
     private YibanOAuth yibanOAuth;
 
     @Autowired
-    private CalendarConfig config;
+    private CalendarConfig calendarConfig;
 
     @RequestMapping("/auth")
     public void doAuth(String vq) {
         try {
-            yibanOAuth.dealYibanOauth(vq, config.appid, config.appkey);
+            yibanOAuth.dealYibanOauth(vq, calendarConfig.appid, calendarConfig.appkey);
         } catch (Exception e) {
             e.printStackTrace();
         }
