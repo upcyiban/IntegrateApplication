ef0359b4f14d013e0d7e5b44b19c0108ed897501
skyADMIN <546485299@qq.com>
2016-07-17 20:45:24 +0800

    add feed back model dao

---
+++ src/main/java/cn/edu/upc/yb/integrate/common/dao/FeedBackMessageDao.java
@@ -0,0 +1,10 @@
+package cn.edu.upc.yb.integrate.common.dao;
+
+import cn.edu.upc.yb.integrate.common.model.FeedBackMessage;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by skyADMIN on 16/7/17.
+ */
+public interface FeedBackMessageDao extends CrudRepository<FeedBackMessage, Integer> {
+}
