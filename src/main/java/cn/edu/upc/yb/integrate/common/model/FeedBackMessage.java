b3e14d734c59e75b1c0afc10bb8993eab42a080c
skyADMIN <546485299@qq.com>
2016-07-17 20:43:29 +0800

    add user feed back model

---
+++ src/main/java/cn/edu/upc/yb/integrate/common/model/FeedBackMessage.java
@@ -0,0 +1,72 @@
+package cn.edu.upc.yb.integrate.common.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * Created by skyADMIN on 16/7/17.
+ * 用于收集用户反馈
+ */
+@Entity
+@Table(name = "common_feedback")
+public class FeedBackMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private int yibanid;
+    private String message;
+    private LocalDateTime sendtime;
+    private String appname;         //反馈错误的应用的名字
+
+    public FeedBackMessage() {
+    }
+
+    public FeedBackMessage(int yibanid, String message, String appname) {
+        this.yibanid = yibanid;
+        this.message = message;
+        this.appname = appname;
+        this.sendtime = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getYibanid() {
+        return yibanid;
+    }
+
+    public void setYibanid(int yibanid) {
+        this.yibanid = yibanid;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getSendtime() {
+        return sendtime;
+    }
+
+    public void setSendtime(LocalDateTime sendtime) {
+        this.sendtime = sendtime;
+    }
+
+    public String getAppname() {
+        return appname;
+    }
+
+    public void setAppname(String appname) {
+        this.appname = appname;
+    }
+}
