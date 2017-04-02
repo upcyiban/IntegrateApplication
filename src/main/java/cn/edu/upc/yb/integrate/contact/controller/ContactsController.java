7b2bc5345386c92c216607fc309aaa955db35a0f
yang liu <lylllcc@outlook.com>
2017-04-02 21:54:44 +0800

    设置但应用管理员

--- src/main/java/cn/edu/upc/yb/integrate/contact/controller/ContactsController.java
+++ src/main/java/cn/edu/upc/yb/integrate/contact/controller/ContactsController.java
@@ -1,5 +1,6 @@
 package cn.edu.upc.yb.integrate.contact.controller;
 
+import cn.edu.upc.yb.integrate.common.dto.YibanBasicUserInfo;
 import cn.edu.upc.yb.integrate.contact.repository.ContactsJobRepository;
 import cn.edu.upc.yb.integrate.contact.repository.ContactsUnitRepository;
 
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by lenovo on 2017/3/25.
@@ -22,9 +25,14 @@ public class ContactsController {
     @Autowired
     ContactsUnitRepository contactsUnitRepository;
 
+    @Autowired
+    private HttpSession httpSession;
+
 
     @RequestMapping(value = "/showunit",method = RequestMethod.GET)
     public Object showUnit(){
+        YibanBasicUserInfo yibanBasicUserInfo = (YibanBasicUserInfo) httpSession.getAttribute("user");
+        System.out.println(yibanBasicUserInfo.visit_user.userid + "123");
         return contactsUnitRepository.findAll();
     }
 
