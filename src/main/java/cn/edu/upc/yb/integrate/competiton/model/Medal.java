fbefc03094d9ace8b32dfe60b8fc41640c24259b
haojun wang <whj9806@gmail.com>
2017-04-13 16:57:18 +0800

    添加验证题目是否完成接口

--- src/main/java/cn/edu/upc/yb/integrate/competiton/model/Medal.java
+++ src/main/java/cn/edu/upc/yb/integrate/competiton/model/Medal.java
@@ -19,10 +19,67 @@ public class Medal {
     private String ybname;
     private int number;
 
+    private boolean finish1=false;
+    private boolean finish2=false;
+    private boolean finish3=false;
+    private boolean finish4=false;
+    private boolean finish5=false;
+
+    public Medal(int yibanId, String ybname, int number, boolean finish1, boolean finish2, boolean finish3, boolean finish4, boolean finish5) {
+        this.yibanId = yibanId;
+        this.ybname = ybname;
+        this.number = number;
+        this.finish1 = finish1;
+        this.finish2 = finish2;
+        this.finish3 = finish3;
+        this.finish4 = finish4;
+        this.finish5 = finish5;
+    }
+
     public Medal(int yibanId, String ybname, int number) {
         this.yibanId = yibanId;
         this.ybname = ybname;
         this.number = number;
+    }
+
+    public boolean isFinish1() {
+        return finish1;
+    }
+
+    public void setFinish1(boolean finish1) {
+        this.finish1 = finish1;
+    }
+
+    public boolean isFinish2() {
+        return finish2;
+    }
+
+    public void setFinish2(boolean finish2) {
+        this.finish2 = finish2;
+    }
+
+    public boolean isFinish3() {
+        return finish3;
+    }
+
+    public void setFinish3(boolean finish3) {
+        this.finish3 = finish3;
+    }
+
+    public boolean isFinish4() {
+        return finish4;
+    }
+
+    public void setFinish4(boolean finish4) {
+        this.finish4 = finish4;
+    }
+
+    public boolean isFinish5() {
+        return finish5;
+    }
+
+    public void setFinish5(boolean finish5) {
+        this.finish5 = finish5;
     }
 
     public Medal() {
