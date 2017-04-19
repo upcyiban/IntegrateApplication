47d13f4cb08a874c1866e1539b138257c8512041
haojun wang <whj9806@gmail.com>
2017-04-19 22:27:34 +0800

    添加通讯录模糊查询

--- src/main/java/cn/edu/upc/yb/integrate/contact/repository/ContactsJobRepository.java
+++ src/main/java/cn/edu/upc/yb/integrate/contact/repository/ContactsJobRepository.java
@@ -3,6 +3,7 @@ package cn.edu.upc.yb.integrate.contact.repository;
 import cn.edu.upc.yb.integrate.contact.model.ContactsJob;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by lenovo on 2017/3/25.
@@ -11,6 +12,6 @@ public interface ContactsJobRepository extends CrudRepository<ContactsJob,Intege
 
     public Iterable<ContactsJob> findByContactsUnitId(int id);
 
-    @Query("select c from ContactsJob c where c.name like %:name%")
-    public Iterable<ContactsJob> findByNameLike(String name);
+    @Query("select c from ContactsJob c where c.name like CONCAT('%',:name,'%')")
+    public Iterable<ContactsJob> findByNameLike(@Param("name") String name);
 }
