b023e90fda6a550d6938268a59b74dd655bbc000
DESKTOP-NQNCHBC\chenzifeng <704734862@qq.com>
2017-04-19 22:58:55 +0800

    重写图片上传接口，调用common包里的方法

--- src/main/java/cn/edu/upc/yb/integrate/deliciousfood/controller/UploadController.java
+++ src/main/java/cn/edu/upc/yb/integrate/deliciousfood/controller/UploadController.java
@@ -4,6 +4,7 @@ import cn.edu.upc.yb.integrate.common.dto.ErrorReporter;
 import cn.edu.upc.yb.integrate.common.dto.YibanBasicUserInfo;
 import cn.edu.upc.yb.integrate.common.service.AppAdminService;
 import cn.edu.upc.yb.integrate.common.service.CommonAdminService;
+import cn.edu.upc.yb.integrate.common.service.FileUploadService;
 import cn.edu.upc.yb.integrate.deliciousfood.dao.VarietyOfDishesDao;
 import cn.edu.upc.yb.integrate.deliciousfood.dto.JsonMes;
 import cn.edu.upc.yb.integrate.deliciousfood.dto.ResultData;
@@ -22,7 +23,7 @@ import java.io.IOException;
  * Created by 陈子枫 on 2017/2/23.
  */
 @RestController
-@RequestMapping(method = RequestMethod.POST,value = "/upload")
+@RequestMapping(value = "/upload")
 public class UploadController {
 
     public static final String ROOT = "imgpath";
@@ -34,62 +35,22 @@ public class UploadController {
     VarietyOfDishesDao varietyOfDishesDao;
 
     @Autowired
-    HttpSession httpSession;
+    FileUploadService fileUploadService;
 
     @Autowired
-    AppAdminService appAdminService;
+    HttpSession httpSession;
 
-    @RequestMapping(value = "/picture", method = RequestMethod.POST)
-    @ResponseBody
-    public Object photoUpload(MultipartFile file, HttpServletRequest request, HttpServletResponse response, HttpSession session,int id) throws IllegalStateException, IOException {
-        ResultData<Object> resultData = new ResultData<>();
-
+    @RequestMapping(method =  RequestMethod.POST,value = "/photo")
+    public Object storePhoto(MultipartFile file,int dishid){
         YibanBasicUserInfo yibanBasicUserInfo =(YibanBasicUserInfo) httpSession.getAttribute("user");
-        //管理员验证
-        if(!appAdminService.isAppAdmin("deliciousfood",yibanBasicUserInfo.visit_user.userid))
-            return new ErrorReporter(-1,"您不是管理员");
-        VarietyOfDishes varietyOfDishes = varietyOfDishesDao.findOne(id);
-        if (file != null) {// 判断上传的文件是否为空
-            varietyOfDishes.setPath(null);// 文件路径
-            String path = null;
-            String fileName = file.getOriginalFilename();// 文件原名称
-            System.out.println("上传的文件原名称:" + fileName);
-            // 判断文件类型,有就给type赋值，没有就为null
-           String  type = fileName.indexOf(".") != -1 ? fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length()) : null;
-            if (type != null) {// 判断文件类型是否为空
-
-                if ("GIF".equals(type.toUpperCase()) || "PNG".equals(type.toUpperCase()) || "JPG".equals(type.toUpperCase())) {
-                    // 项目在容器中实际发布运行的根路径
-                    String realPath = request.getSession().getServletContext().getRealPath("/");
-                    // 自定义的文件名称
-                    String trueFileName = String.valueOf(System.currentTimeMillis()) + fileName;
-                    // 设置存放图片文件的路径
-                   varietyOfDishes.setPath(realPath +/*System.getProperty("file.separator")+*/trueFileName);
-                    System.out.println("存放图片文件的路径:" + varietyOfDishes.getPath());
-                    // 转存文件到指定的路径
-                    file.transferTo(new File(varietyOfDishes.getPath()));
-                    System.out.println("文件成功上传到指定目录下");
-                    varietyOfDishesDao.save(varietyOfDishes);
-
-                } else {
-                    System.out.println("不是我们想要的文件类型,请按要求重新上传");
-                    return new ErrorReporter(-1,"上传失败");
-
-                }
-
-            } else {
-                System.out.println("文件类型为空");
-                return new ErrorReporter(-1,"文件类型为空");
-
-            }
-
-        } else {
-            System.out.println("没有找到相对应的文件");
-            return new ErrorReporter(-1,"没有找到相对应的文件");
-
-        }
-        return new JsonMes(1,"上传成功" );
-
+        String name = "deliciousfood"+ yibanBasicUserInfo.visit_user.userid+System.currentTimeMillis();
+        VarietyOfDishes varietyOfDishes = varietyOfDishesDao.findOne(dishid);
+        if(varietyOfDishes == null)
+            return new JsonMes(-1,"未找到");
+        varietyOfDishes.setPath("file/img/"+name);
+        varietyOfDishesDao.save(varietyOfDishes);
+        fileUploadService.store(file,name);
+        return new JsonMes(1,"上传成功");
     }
 
 }
