3a83a3818502680fe4a675063ca280160ef3d012
sky-ADMIN <546485299@qq.com>
2017-04-28 20:07:55 +0800

    去掉无用的import

--- src/main/java/cn/edu/upc/yb/integrate/InstructorEvaluate/controller/InstructorEvaluateController.java
+++ src/main/java/cn/edu/upc/yb/integrate/InstructorEvaluate/controller/InstructorEvaluateController.java
@@ -1,9 +1,6 @@
 package cn.edu.upc.yb.integrate.InstructorEvaluate.controller;
 
-import cn.edu.upc.yb.integrate.InstructorEvaluate.model.Student;
 import cn.edu.upc.yb.integrate.InstructorEvaluate.service.InstructorService;
-import cn.edu.upc.yb.integrate.common.util.JsonWebToken;
-import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
